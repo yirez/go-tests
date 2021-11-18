ea696f632ce1a7e1971e414e1d7c07125f3ba148
yirez <irezyigit@gmail.com>
2021-11-18 23:03:23 +0300

    added channel tests

---
+++ threads/channels.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var waitGroup2 sync.WaitGroup
+var channelInt = make(chan int)
+
+func main() {
+
+	waitGroup2.Add(2)
+	go generateNum()
+	go yell2()
+	fmt.Printf("rand: %v \n", <-channelInt)
+	close(channelInt)
+	waitGroup2.Wait()
+	fmt.Printf("%v go routines finished\n", runtime.NumGoroutine())
+}
+
+func generateNum() {
+	rand.Seed(time.Now().UnixNano())
+	channelInt <- rand.Intn(100-10) + 10
+	waitGroup2.Done()
+}
+
+func yell2() {
+	defer waitGroup2.Done()
+	for i := 0; i < 3; i++ {
+		fmt.Printf("YELLING HI, %v times\n", i)
+	}
+}
